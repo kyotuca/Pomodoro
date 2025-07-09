ea8429cdb7ec5a992d0725f413291b0b6abb30f0
Francesco Montefusco <francesco.montefusco@mail.polimi.it>
2025-07-09 23:45:14 +0200

    ci: added basic actions

---
+++ .commitlint.config.mjs
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum' : [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'perf',
+                'docs',
+                'chore',
+                'ci',
+                'build'
+            ]
+        ],
+        'scope-empty': [2, 'never'],
+        'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+        'header-max-length': [2, 'always', 100]
+    }
+};
